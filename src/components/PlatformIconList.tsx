a9951d78a87a44d10504bce689f6af1e786253ab
Jay Elbourne <jayelbourne@gmail.com>
2023-03-14 23:38:51 -0400

    Added platform icons to game cards

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,35 @@
+import { FaWindows, FaPlaystation, FaXbox, FaApple, FaLinux, FaAndroid } from 'react-icons/fa';
+import { MdPhoneIphone} from 'react-icons/md';
+import { SiNintendo } from 'react-icons/si';
+import {BsGlobe } from 'react-icons/bs';
+import { HStack, Icon } from '@chakra-ui/react';
+import { Platform } from "../hooks/useGames";
+import { IconType } from 'react-icons';
+
+interface Props {
+    platforms: Platform[];
+}
+
+const PlatformIconList = ({ platforms }: Props) => {
+    const iconMap: { [key: string]: IconType  } = {
+        playstation: FaPlaystation,
+        xbox: FaXbox,
+        pc: FaWindows,
+        nintendo: SiNintendo,
+        ios: MdPhoneIphone,
+        web: BsGlobe,
+        linux: FaLinux,
+        mac: FaApple,
+        abdroid: FaAndroid,
+    }
+
+    return (
+        <HStack marginY={2}>
+       {platforms.map((platform) => (
+            <Icon as={iconMap[platform.slug]} color='gray.500' />
+       ))}
+        </HStack>
+    );
+}
+
+export default PlatformIconList
