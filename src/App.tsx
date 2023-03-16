1526e78028333651520c373cf21ee86dc11d0567
Jay Elbourne <jayelbourne@gmail.com>
2023-03-16 14:08:59 -0400

    Filter games by genre

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
 import { Grid, GridItem, Show } from '@chakra-ui/react'
+import { useState } from 'react';
 import GameGrid from './components/GameGrid';
 import GenreList from './components/GenreList';
 import NavBar from './components/Navbar';
+import { Genre } from './hooks/useGenres';
 
 function App() {
+    const [selectedGenre, setSelectedGenre] = useState<Genre| null>(null);
     return (
     <Grid templateAreas={{
             base: `"nav" "main"`,
             lg: `"nav nav" "aside main"` // 1024px
-        }}>
+        }}
+        templateColumns={{
+            base: '1fr',
+            lg: '200px 1fr'
+            }}
+        >
         <GridItem area='nav'>
             <NavBar />
         </GridItem>
         <Show above="lg">
-            <GridItem area='aside'>
-                <GenreList />
+            <GridItem area='aside' paddingX='5px'>
+                <GenreList onSelectedGenre={(genre) => setSelectedGenre(genre)}/>
             </GridItem>
         </Show>
         <GridItem area='main'>
-            <GameGrid />
+            <GameGrid selectedGenre={selectedGenre}/>
         </GridItem>
     </Grid>
     );
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,12 +1,29 @@
-import useGenres from "../hooks/useGenres";
+import { HStack, List, ListItem, Image, Button, Spinner } from "@chakra-ui/react";
+import useGenres, { Genre } from "../hooks/useGenres";
+import getCroppedImageUrl from "../services/image-url";
 
+interface Props {
+    onSelectedGenre: (genre: Genre) => void;
+}
 
-const GenreList = () => {
-    const { data } = useGenres();
+const GenreList = ( { onSelectedGenre} : Props) => {
+    const { data, isLoading, error } = useGenres();
+
+    if (error) return null;
+    if (isLoading) return <Spinner />;
     return (
-        <ul>
-            {data.map(genre => <li key={genre.id}>{genre.name}</li>)}
-        </ul>
+        <List>
+            {data.map(genre => (
+                <ListItem key={genre.id} paddingY='5px'>
+                    <HStack>
+                        <Image
+                            boxSize="32px" 
+                            borderRadius={8}
+                            src={getCroppedImageUrl(genre.image_background)} />
+                        <Button onClick={() => onSelectedGenre(genre)} fontSize='lg' variant='link'>{genre.name}</Button>
+                    </HStack>
+                </ListItem>))}
+        </List>
     );
 }
 
