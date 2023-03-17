a1d273406a9d946125aad83cfef6ef805c97ac6d
Jay Elbourne <jayelbourne@gmail.com>
2023-03-16 22:39:43 -0400

    IOmplement searching

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ export interface GameQuery {
     genre: Genre | null;
     platform: Platform | null;
     sortOrder: string;
+    searchText: string;
 }
 
 
@@ -30,7 +31,10 @@ function App() {
             }}
         >
             <GridItem area='nav'>
-                <NavBar />
+                <NavBar onSearch={
+                    (searchText) => (
+                        setGameQuery({ ...gameQuery, searchText })
+                    )} />
             </GridItem>
             <Show above="lg">
                 <GridItem area='aside' paddingX='5px'>
